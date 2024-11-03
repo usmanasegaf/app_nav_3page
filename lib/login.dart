4fabe4f8ab341bb3105877c5d661529ee719a668
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-11-03 19:40:17 +0700

    login, main, mainpage

--- lib/login.dart
+++ lib/login.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:app_nav_3page/mainpage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+final userNameController = TextEditingController();
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Aplikasi Biodata"),
+      ),
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) {
-                return const MainPage();
-              }));
-            },
-            child: const Text("Masuk")),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Username'),
+                controller: userNameController,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      print(userNameController.text);
+
+                      if (userNameController.text == "Mohammad Usman Asegaf") {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const MainPage();
+                        }));
+                      } else if (userNameController.text == "") {
+                        Fluttertoast.showToast(
+                            msg: "Username tidak boleh kosong",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 2,
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                      } else
+                        (Fluttertoast.showToast(
+                            msg: "Username Tidak Terdaftar",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 2,
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0));
+                    },
+                    child: const Text("Masuk")),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,3 +1,4 @@
+import 'package:app_nav_3page/login.dart';
 import 'package:app_nav_3page/secondpage.dart';
 import 'package:flutter/material.dart';
 
@@ -6,18 +7,28 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String userName = userNameController.text;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Main Page"),
       ),
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const SecondPage();
-              }));
-            },
-            child: const Text("Pindah Halaman")),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const SecondPage();
+                  }));
+                },
+                child: const Text("Pindah Halaman")),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text("Selamat Datang " + userName),
+            ),
+          ],
+        ),
       ),
     );
   }
