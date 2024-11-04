609ccd24bb38789b3c388ab7c9bf5ba6f4a4812b
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2024-11-04 13:50:50 +0700

    Mohammad Usman Asegaf Bio

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,6 +1,7 @@
 import 'package:app_nav_3page/login.dart';
 import 'package:app_nav_3page/secondpage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -12,23 +13,57 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Main Page"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const SecondPage();
-                  }));
-                },
-                child: const Text("Pindah Halaman")),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text("Selamat Datang " + userName),
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/background_1.svg',
+            alignment: Alignment.bottomCenter,
+            width: 2560.0,
+            height: 1440.0,
+            fit: BoxFit.scaleDown,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/foto_profil_rb.jpg',
+                        height: 220.0,
+                        width: 220.0,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const SecondPage();
+                        }));
+                      },
+                      child: const Text(
+                        "Pindah Halaman",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Selamat Datang " + userName,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
@@ -9,12 +10,51 @@ class SecondPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Second Page"),
       ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text("Back")),
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/background_1.svg',
+            alignment: Alignment.bottomCenter,
+            width: 2560.0,
+            height: 1440.0,
+            fit: BoxFit.scaleDown,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ClipOval(
+                      child: Image.asset(
+                    'assets/foto_profil_rb.jpg',
+                    height: 220.0,
+                    width: 220.0,
+                  )),
+                ),
+                const Text(
+                  "Nama:    Mohammad Usman Asegaf\n" +
+                      "Hobi:    Olahraga\n" +
+                      "TTL:   Bandung, 5 Juni 2002\n" +
+                      "Alamat:    Cimahi selatan, Perumnas Gg Nusa Indah 4 No 29\n" +
+                      "Universitas:   Universitas Teknologi Bandung\n" +
+                      "Jurusan:   Teknik Informatika\n" +
+                      "Pengalaman:    SR Offset(Logistik), Remotask(QA, Reviewer)\n",
+                  textAlign: TextAlign.start,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Back",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    )),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
